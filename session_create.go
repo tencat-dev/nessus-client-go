48c5d5f304b4001fabdb4d62df923b208532cf33
anhnmt <anhng.dev@gmail.com>
2025-09-22 19:40:29 +0700

    Add session create

---
+++ session_create.go
@@ -0,0 +1,47 @@
+package nessus
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/bytedance/sonic"
+)
+
+type SessionCreateRequest struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+type SessionCreateResponse struct {
+	Md5SumWizardTemplates string `json:"md5sum_wizard_templates,omitempty"`
+	Token                 string `json:"token,omitempty"`
+	Md5SumTenableLinks    string `json:"md5sum_tenable_links,omitempty"`
+}
+
+func (c *Client) SessionCreate(req *SessionCreateRequest) (*SessionCreateResponse, error) {
+	reqBody, err := sonic.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.req.Post(c.apiURL+"/session", "application/json", reqBody)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, ErrorResponse(body)
+	}
+
+	var data SessionCreateResponse
+	if err = sonic.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
