a81f6268a135060efd979dbc487728cb76c56825
mizhm <minhnt141204@gmail.com>
2025-09-27 16:09:41 +0700

    feat(scan): add ScansExportStatus

--- nessus/scans_details.go
+++ nessus/scans_details.go
@@ -146,6 +146,7 @@ func (c *Client) ScansDetails(id int, query *ScansDetailsQuery) (*ScansDetailsRe
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
