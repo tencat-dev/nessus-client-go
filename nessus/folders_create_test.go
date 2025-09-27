ad4a8ab2687a5758c6a5ec7945e9a75f632c1032
anhnmt <anhng.dev@gmail.com>
2025-09-27 16:19:11 +0700

    feat: add unittest folders create

---
+++ nessus/folders_create_test.go
@@ -0,0 +1,53 @@
+package nessus
+
+import (
+	"testing"
+)
+
+func TestClient_FoldersCreate(t *testing.T) {
+	tests := []struct {
+		name    string
+		options []Option
+		req     *FoldersCreateRequest
+		wantErr bool
+	}{
+		{
+			name: "success",
+			options: []Option{
+				WithAPIURL("https://localhost:8834"),
+				WithAPIKey(
+					"ede55bc4fbad66a41a46f4a5ff35500e7485adae1bc5d94d98e9a1c1f7bb0ecc",
+					"af2ca8def3fa67705e38ded764d3c282fb7f82a516883bb4f1e310aba02f1e1b",
+				),
+			},
+			req: &FoldersCreateRequest{
+				Name: "test",
+			},
+			wantErr: false,
+		},
+		{
+			name: "error",
+			options: []Option{
+				WithAPIURL("https://localhost:8834"),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := NewClient(tt.options...)
+			if err != nil {
+				t.Errorf("NewClient() error = %v", err)
+			}
+
+			got, err := c.FoldersCreate(tt.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FoldersCreate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != nil && got.ID == 0 {
+				t.Errorf("FoldersCreate() got = %v", got)
+			}
+		})
+	}
+}
